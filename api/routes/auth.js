6b351c13e2b059277cb9fabc60580edd16d5c0f5
vnavarro <vnavarro@brightcove.com>
2020-11-01 14:52:09 -0600

    Add endpoint for user data

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
 const Users = require('../models/Users')
+const { isAuthenticated } = require('../auth')
 
 const router = express.Router()
 
@@ -52,6 +53,9 @@ router.post('/login', (req, res) => {
 		})
 })
 
+router.get('/me', isAuthenticated, (req, res) => {
+	res.send(req.user)
+})
 
 module.exports = router
 
